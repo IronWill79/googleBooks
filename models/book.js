6ebd2d15195eb84d10fe6dc2b63d99d2bb644add
rashawn doyley <rashawndoyley@gmail.com>
2021-04-23 01:33:36 -0400

    new update

--- models/book.js
+++ models/book.js
@@ -5,7 +5,7 @@ const bookSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     description: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: String },
     date: { type: Date, default: Date.now },
 });
 
